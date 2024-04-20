33205890411615e2b39e68b008fe2cc2a8620e8b
doomdabo <bbo1209@naver.com>
2024-04-20 11:20:23 +0900

    feat: 프로젝트 신규 생성
    프로젝트 신규생성하였습니다.

---
+++ backend/src/test/java/chaldduck/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package chaldduck.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
