6f525317796a93770867bace2d9e81562824e0fe
EllieHyun <hyunyelim25@gmail.com>
2024-05-22 21:36:18 +0900

    feat : domain 생성
    - saju
    - user
    - sajuCompatabilityData
    도메인 생성

---
+++ backend/src/main/java/chaldduck/backend/src/domain/User.java
@@ -0,0 +1,27 @@
+package chaldduck.backend.src.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @OneToOne
+    private Saju saju;
+
+    @Column(name = "birth", nullable = false)
+    private Date birth;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "mbti", nullable = false)
+    private String mbti;
+}
