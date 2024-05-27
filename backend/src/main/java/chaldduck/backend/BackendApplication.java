35557418ec71b8f4aece2c0fcbab6de98e774e0e
EllieHyun <hyunyelim25@gmail.com>
2024-05-27 22:24:30 +0900

    feat : add data
    add data
    save test data to repository

--- backend/src/main/java/chaldduck/backend/BackendApplication.java
+++ backend/src/main/java/chaldduck/backend/BackendApplication.java
@@ -2,14 +2,19 @@ package chaldduck.backend;
 
 import chaldduck.backend.src.domain.MbtiAnswer;
 import chaldduck.backend.src.domain.MbtiQuestion;
-import chaldduck.backend.src.domain.Users;
+import chaldduck.backend.src.repository.MbtiAnswerRepository;
+import chaldduck.backend.src.repository.MbtiQuestionRepository;
 import jakarta.annotation.PostConstruct;
-import org.apache.catalina.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class BackendApplication {
+
+	private final MbtiAnswerRepository mbtiAnswerRepository;
+	private final MbtiQuestionRepository mbtiQuestionRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -34,6 +39,9 @@ public class BackendApplication {
 				.answer("당장 친구들 불러! 스트레스 풀자!~") //e
 				.score(1)
 				.build();
+		mbtiQuestionRepository.save(mbtiQuestion1);
+		mbtiAnswerRepository.save(mbtiAnswer1);
+		mbtiAnswerRepository.save(mbtiAnswer2);
 		////////////////////////////////////////
 		MbtiQuestion mbtiQuestion2 = MbtiQuestion.builder()
 				.id(2)
@@ -52,6 +60,9 @@ public class BackendApplication {
 				.answer("내가 먼저 친해지자며 말건다") //e
 				.score(1)
 				.build();
+		mbtiQuestionRepository.save(mbtiQuestion2);
+		mbtiAnswerRepository.save(mbtiAnswer3);
+		mbtiAnswerRepository.save(mbtiAnswer4);
 		////////////////////////////////////////
 		MbtiQuestion mbtiQuestion3 = MbtiQuestion.builder()
 				.id(3)
@@ -70,6 +81,9 @@ public class BackendApplication {
 				.answer("내가 먼저 친해지자며 말건다") //e
 				.score(1)
 				.build();
+		mbtiQuestionRepository.save(mbtiQuestion3);
+		mbtiAnswerRepository.save(mbtiAnswer5);
+		mbtiAnswerRepository.save(mbtiAnswer6);
 		////////////////////////////////////////
 		MbtiQuestion mbtiQuestion4 = MbtiQuestion.builder()
 				.id(4)
@@ -88,6 +102,9 @@ public class BackendApplication {
 				.answer("스스로 나만의 방법을 만들어 나가는게 좋다") //n
 				.score(1)
 				.build();
+		mbtiQuestionRepository.save(mbtiQuestion4);
+		mbtiAnswerRepository.save(mbtiAnswer7);
+		mbtiAnswerRepository.save(mbtiAnswer8);
 		////////////////////////////////////////
 		MbtiQuestion mbtiQuestion5 = MbtiQuestion.builder()
 				.id(5)
@@ -106,6 +123,9 @@ public class BackendApplication {
 				.answer("\"떨어지면 어떡하지? 나무에 떨어지면 살 수 있을까?\"라며 상상의 나래를 펼친다 ") //n
 				.score(1)
 				.build();
+		mbtiQuestionRepository.save(mbtiQuestion5);
+		mbtiAnswerRepository.save(mbtiAnswer9);
+		mbtiAnswerRepository.save(mbtiAnswer10);
 		////////////////////////////////////////
 		MbtiQuestion mbtiQuestion6 = MbtiQuestion.builder()
 				.id(6)
@@ -124,7 +144,134 @@ public class BackendApplication {
 				.answer("창의적이고 독창적이다") //n
 				.score(1)
 				.build();
+		mbtiQuestionRepository.save(mbtiQuestion6);
+		mbtiAnswerRepository.save(mbtiAnswer11);
+		mbtiAnswerRepository.save(mbtiAnswer12);
 		////////////////////////////////////////
+		MbtiQuestion mbtiQuestion7 = MbtiQuestion.builder()
+				.id(7)
+				.kind("TF")
+				.question("회사에서 내가 더 듣고싶은 말은?")
+				.build();
+		MbtiAnswer mbtiAnswer13 = MbtiAnswer.builder()
+				.id(13)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("\"성격은 별론데 일 하나는 똑부러지게 잘해~\"") //t
+				.score(-1)
+				.build();
+		MbtiAnswer mbtiAnswer14 = MbtiAnswer.builder()
+				.id(14)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("\"일은 좀 못해도 사람은 참 좋아~\"") //f
+				.score(1)
+				.build();
+		mbtiQuestionRepository.save(mbtiQuestion7);
+		mbtiAnswerRepository.save(mbtiAnswer13);
+		mbtiAnswerRepository.save(mbtiAnswer14);
+		////////////////////////////////////////
+		MbtiQuestion mbtiQuestion8 = MbtiQuestion.builder()
+				.id(8)
+				.kind("TF")
+				.question("친구랑 아주 슬픈 영화를 보러 갔다. 영화가 끝나고 나는")
+				.build();
+		MbtiAnswer mbtiAnswer15 = MbtiAnswer.builder()
+				.id(15)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("나라면 저렇게 안했을텐데.. 왜 울지?") //t
+				.score(-1)
+				.build();
+		MbtiAnswer mbtiAnswer16 = MbtiAnswer.builder()
+				.id(16)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("영화가 너무 슬펐어..") //f
+				.score(1)
+				.build();
+		mbtiQuestionRepository.save(mbtiQuestion8);
+		mbtiAnswerRepository.save(mbtiAnswer15);
+		mbtiAnswerRepository.save(mbtiAnswer16);
+		////////////////////////////////////////
+		MbtiQuestion mbtiQuestion9 = MbtiQuestion.builder()
+				.id(9)
+				.kind("TF")
+				.question("\"나 일 못한다고 상사한테 까였어ㅠㅠ\"라고 말하는 친구에게 나는")
+				.build();
+		MbtiAnswer mbtiAnswer17 = MbtiAnswer.builder()
+				.id(17)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("너가 뭘 잘못했는데? 큰 실수했어?") //t
+				.score(-1)
+				.build();
+		MbtiAnswer mbtiAnswer18 = MbtiAnswer.builder()
+				.id(18)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("헐 괜찮아? 그 상사 너무하네~") //f
+				.score(1)
+				.build();
+		mbtiQuestionRepository.save(mbtiQuestion9);
+		mbtiAnswerRepository.save(mbtiAnswer17);
+		mbtiAnswerRepository.save(mbtiAnswer18);
+		////////////////////////////////////////
+		MbtiQuestion mbtiQuestion10 = MbtiQuestion.builder()
+				.id(10)
+				.kind("JP")
+				.question("오늘은 친구와의 약속이 있는 날! 약속 한시간 전 나는")
+				.build();
+		MbtiAnswer mbtiAnswer19 = MbtiAnswer.builder()
+				.id(19)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("우리 어디서 몇시에 볼래?") //p
+				.score(-1)
+				.build();
+		MbtiAnswer mbtiAnswer20 = MbtiAnswer.builder()
+				.id(20)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("그때 예약해둔 맛집 앞에서 12까지 봐~") //j
+				.score(1)
+				.build();
+		mbtiQuestionRepository.save(mbtiQuestion10);
+		mbtiAnswerRepository.save(mbtiAnswer19);
+		mbtiAnswerRepository.save(mbtiAnswer20);
+		////////////////////////////////////////
+		MbtiQuestion mbtiQuestion11 = MbtiQuestion.builder()
+				.id(11)
+				.kind("JP")
+				.question("친구가 우리집에 놀러와서 가위를 찾는다!")
+				.build();
+		MbtiAnswer mbtiAnswer21 = MbtiAnswer.builder()
+				.id(21)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("음 어디다 뒀지? 책상 한번 봐볼래?") //p
+				.score(-1)
+				.build();
+		MbtiAnswer mbtiAnswer22 = MbtiAnswer.builder()
+				.id(22)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("그거 왼쪽 서랍 세번째 칸에 있어") //j
+				.score(1)
+				.build();
+		mbtiQuestionRepository.save(mbtiQuestion11);
+		mbtiAnswerRepository.save(mbtiAnswer21);
+		mbtiAnswerRepository.save(mbtiAnswer22);
+		////////////////////////////////////////
+		MbtiQuestion mbtiQuestion12 = MbtiQuestion.builder()
+				.id(12)
+				.kind("JP")
+				.question("과제 제출 마감 30분 전! 나는")
+				.build();
+		MbtiAnswer mbtiAnswer23 = MbtiAnswer.builder()
+				.id(23)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("막판 스퍼트! 1분 1초도 놓칠 수 없어!") //p
+				.score(-1)
+				.build();
+		MbtiAnswer mbtiAnswer24 = MbtiAnswer.builder()
+				.id(24)
+				.mbtiQuestion(mbtiQuestion2)
+				.answer("이거 끝났어! 내가 미리 다 해놨지~") //j
+				.score(1)
+				.build();
+		mbtiQuestionRepository.save(mbtiQuestion12);
+		mbtiAnswerRepository.save(mbtiAnswer23);
+		mbtiAnswerRepository.save(mbtiAnswer24);
 	}
-
 }
