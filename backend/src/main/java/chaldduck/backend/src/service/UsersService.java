a92bcf2b5b0df29d1394c0ced82cf8e6299a9f45
EllieHyun <hyunyelim25@gmail.com>
2024-08-03 16:15:04 +0900

    mbti null 에러 해결

--- backend/src/main/java/chaldduck/backend/src/service/UsersService.java
+++ backend/src/main/java/chaldduck/backend/src/service/UsersService.java
@@ -1,5 +1,4 @@
 package chaldduck.backend.src.service;
-import chaldduck.backend.global.enums.Mbti;
 
 import chaldduck.backend.src.domain.Saju;
 import chaldduck.backend.src.domain.Users;
@@ -17,12 +16,16 @@ public class UsersService {
     private final UsersRepository usersRepository;
 
     public Users saveUser(UsersInfoRequestDTO usersInfoRequestDTO) {
+        // TODO 생년월일 기반으로 오행 정보 가져와서 넣어줘야함
+
+
         Users user = Users.builder()
                 .nickname(usersInfoRequestDTO.getNickname())
                 .birth(usersInfoRequestDTO.getBirth())
                 .saju(Saju.builder()
                         .fiveHang("갑자")
                         .build())
+                .mbti("mbti")
                 .build();
         return usersRepository.save(user);
     }
